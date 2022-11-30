a6012cbe2fbaa9ac9350dc20d6df039b1f7750de
Ashish Khanal <AKhanal@marathonpetroleum.com>
2022-11-30 17:38:00 -0500

    Adds user management pages

---
+++ src/HMT.Web.Server/Features/ManageUsers/EditUserPage.razor.cs
@@ -0,0 +1,77 @@
+﻿using HMT.Web.Server.Areas.Identity;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HMT.Web.Server.Features.ManageUsers
+{
+    public partial class EditUserPage : ComponentBase
+    {
+        [Parameter]
+        public string UserId { get; set; } = default!;
+
+        [Inject]
+        public UserManager<HMTUser> UserManager { get; set; } = default!;
+
+        [Inject]
+        public RoleManager<IdentityRole> RoleManager { get; set; } = default!;
+
+        [Inject]
+        public NavigationManager Navigation { get; set; } = default!;
+
+        public HMTUser User { get; set; } = default!;
+
+        public ICollection<IdentityRole> Roles { get; set; } = new List<IdentityRole>();
+        public ICollection<string> UserRoles { get; set; } = new List<string>();
+
+        protected override async Task OnParametersSetAsync()
+        {
+            Roles = await RoleManager.Roles.ToListAsync();
+
+            User = await UserManager.FindByIdAsync(UserId);
+
+            UserRoles = await UserManager.GetRolesAsync(User);
+        }
+
+        public void ToggleSelectedRole(string roleName)
+        {
+            if (UserRoles.Contains(roleName))
+            {
+                UserRoles.Remove(roleName);
+            }
+            else
+            {
+                UserRoles.Add(roleName);
+            }
+
+            StateHasChanged();
+        }
+
+        public async Task UpdateUser()
+        {
+            var user = await UserManager.FindByIdAsync(User.Id);
+            if (user == null) return;
+
+            user.UserName = User.UserName;
+            user.Email = User.Email;
+
+            await UserManager.UpdateAsync(user);
+
+            var currentRoles = await UserManager.GetRolesAsync(user);
+            var addedRoles = UserRoles.Except(currentRoles);
+            var removedRoles = currentRoles.Except(UserRoles);
+
+            if (addedRoles.Any())
+            {
+                await UserManager.AddToRolesAsync(user, addedRoles);
+            }
+
+            if (removedRoles.Any())
+            {
+                await UserManager.RemoveFromRolesAsync(user, removedRoles);
+            }
+
+            Navigation.NavigateTo("/view/users");
+        }
+    }
+}
---
+++ src/HMT.Web.Server/Features/ManageUsers/ViewUsersPage.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using HMT.Web.Server.Areas.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HMT.Web.Server.Features.ManageUsers
+{
+    public partial class ViewUsersPage : ComponentBase
+    {
+        [Inject]
+        public UserManager<HMTUser> UserManager { get; set; } = default!;
+        public ICollection<HMTUser> Users { get; set; } = new List<HMTUser>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Users = await UserManager.Users.ToListAsync();
+        }
+    }
+}
