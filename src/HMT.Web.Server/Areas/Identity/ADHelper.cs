a66e5dcd3eadffa0ade5caedee39b68e924e27d9
Ashish Khanal <AKhanal@marathonpetroleum.com>
2022-12-20 11:31:06 -0500

    Adds page to show Active Directory information of a user

--- src/HMT.Web.Server/Areas/Identity/ADHelper.cs
+++ src/HMT.Web.Server/Areas/Identity/ADHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.DirectoryServices.AccountManagement;
+﻿using HMT.Web.Server.Features.UserInformation;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
 
 namespace HMT.Web.Server.Areas.Identity
 {
@@ -10,6 +12,52 @@ namespace HMT.Web.Server.Areas.Identity
             bool isValidLogin = principalContext.ValidateCredentials(userName.ToUpper(), password);
 
             return isValidLogin;
+        }
+
+        // Reference: https://stackoverflow.com/a/5310317/8644294
+        public static UserInfo GetADUserInfo(string userName)
+        {
+            UserInfo userInfo = new ();
+            userInfo.UserName = userName.ToUpper();
+
+            // if you do repeated domain access, you might want to do this *once* outside this method, 
+            // and pass it in as a second parameter!
+            using PrincipalContext myDomain = new(ContextType.Domain);
+
+            // find your user
+            UserPrincipal user = UserPrincipal.FindByIdentity(myDomain, userName);
+
+            // if found - grab its groups
+            if (user != null)
+            {
+                PrincipalSearchResult<Principal> groups = user.GetAuthorizationGroups();
+
+                // iterate over all groups
+                foreach (Principal p in groups)
+                {
+                    // make sure to add only group principals
+                    if (p is GroupPrincipal)
+                    {
+                        userInfo.GroupNames.Add(p.Name);
+                    }
+                }
+
+                // Populate rest of the properties you need
+                userInfo.FirstName = user.GivenName;
+                userInfo.LastName = user.Surname;
+
+                DirectoryEntry de = (user.GetUnderlyingObject() as DirectoryEntry);
+
+                if (de != null)
+                {
+                    if (de.Properties.Contains("department"))
+                    {
+                        userInfo.DepartmentName = de.Properties["department"][0].ToString();
+                    }
+                }
+            }
+
+            return userInfo;
         }
     }
 }
---
+++ src/HMT.Web.Server/Features/UserInformation/UserInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace HMT.Web.Server.Features.UserInformation
+{
+    public class UserInfo
+    {
+        private string? departmentName;
+
+        public string UserName { get; set; } = default!;
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public List<string> GroupNames { get; set; } = new();
+        public string CommaSeparatedGroupNames
+        {
+            get => string.Join(",", GroupNames);
+        }
+
+        // Noticed that this property comes with lots of whitespaces, so decided to trim them. For eg: "Grp1         "
+        public string? DepartmentName
+        {
+            get => departmentName;
+            set => departmentName = value?.Trim();
+        }
+    }
+}
