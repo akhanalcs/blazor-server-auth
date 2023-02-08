fa30e1c087099a641f320ebd9238238feac731bf
Ashish Khanal <AKhanal@marathonpetroleum.com>
2023-02-08 16:17:30 -0500

    Fixes the Migration files

--- src/HMT.Web.Server/Data/Migrations/20221202184331_Adds Permissions column to Roles table.cs
+++ src/HMT.Web.Server/Data/Migrations/20221202184331_Adds Permissions column to Roles table.cs
@@ -8,9 +8,6 @@ namespace HMT.Web.Server.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "RepairOrders");
-
             migrationBuilder.AddColumn<int>(
                 name: "Permissions",
                 table: "AspNetRoles",
@@ -24,20 +21,6 @@ namespace HMT.Web.Server.Data.Migrations
             migrationBuilder.DropColumn(
                 name: "Permissions",
                 table: "AspNetRoles");
-
-            migrationBuilder.CreateTable(
-                name: "RepairOrders",
-                columns: table => new
-                {
-                    OrderId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    SomeUniqueThingInDb = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_RepairOrders", x => x.OrderId);
-                });
         }
     }
 }
