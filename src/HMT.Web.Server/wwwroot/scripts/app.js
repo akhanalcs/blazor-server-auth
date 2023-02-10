5577652da17a567bd31b4e8d59220aa0b06aa952
Ashish Khanal <AKhanal@marathonpetroleum.com>
2023-02-10 16:26:04 -0500

    Logout a user programmatically

---
+++ src/HMT.Web.Server/wwwroot/scripts/app.js
@@ -0,0 +1,5 @@
+﻿var app = {};
+
+app.ClickLogOutButton = function () {
+    document.getElementById("logout-button").click(); /*logout-button is in Areas/Identity/Components/LoginDisplay.razor*/
+}
